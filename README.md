14f43d0214db5198f0813da3062d140aab857b24
Erik Eriksson Broomé <erik.o.eriksson@telia.com>
2025-03-07 13:37:29 +0100

    changed and added comments

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+Creator : Erik Eriksson Broomé
+
+Github repository-page: https://github.com/Grayfox665/Uppgift3-Dynamisk-sida
+
 This is the third school project, wich builds on the second school project.
 
 The additional coding was built with Visual Studio Code by usin Javascript.
@@ -7,3 +11,4 @@ The other project cards are simple placeholder for future project to be placed i
 
 Haven't put much info about me, but that will change later in time. For now there is lorem ipsum to act as placeholder.
 
+Have coded in functions into the main page like a slideshow so you can slide through the project cards with help  of the buttons to their sides, and also added a hidden easter egg.
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 
-/*Creates unordered list for CV */
+/*Creates unordered list for CV and Ids them */
 const ul = document.createElement('ul');
 const ulTwo = document.createElement('ul');
 
@@ -14,6 +14,7 @@ ulTwo.setAttribute('id', 'CVeducation');
 /*Selects profile-article to give Id */
 document.querySelector("article.profile-article").setAttribute('id', 'CV-article');
 
+/*inserts unordered list into article parent */
 document.getElementById('CV-article').appendChild(ul);
 document.getElementById('CV-article').appendChild(br);
 document.getElementById('CV-article').appendChild(ulTwo);
@@ -47,8 +48,9 @@ function fetchData() {
                 educationList.appendChild(liTwo);
             });
         })
+        /*Creates an console error incase failure to fetch JSON files  */
         .catch(function(error) {
-            console.error("Fel vid hamtning av JSON:", error);
+            console.error("Fel vid hämtning av JSON:", error);
         });
 }
 
@@ -66,11 +68,12 @@ document.getElementById('CVeducation').style.listStyle = "none";
 
 /*easteregg interaktive funktion */
 
-/*creats the easterbtn */
+/*creates the easterbtn and Ids it */
 const easterBtn = document.createElement('label');
 
 easterBtn.setAttribute('id', "Easter-button");
 
+/*inserts the easterbtn into parent Element, aswell Ids the parent Element and styles it's position */
 document.querySelector(".picture-box").appendChild(easterBtn);
 document.querySelector(".picture-box").setAttribute('id', "CV-picture-box");
 document.getElementById("CV-picture-box").style.position = "relative";
@@ -127,6 +130,7 @@ window.onclick = function(event) {
 
 /* slideshow function  */
 
+/*creates arrow Elements and sets id */
 const arrowDiv = document.createElement('div');
 const arrowLeft = document.createElement('button');
 const arrowRight = document.createElement('button');
@@ -135,6 +139,7 @@ arrowDiv.setAttribute("id", "slide-arrow-container");
 arrowLeft.setAttribute("id", "slide-arrow-left");
 arrowRight.setAttribute("id", "slide-arrow-right");
 
+/*inserts into parent Element */
 document.querySelector(".Projekts").appendChild(arrowDiv);
 document.getElementById("slide-arrow-container").appendChild(arrowLeft);
 document.getElementById("slide-arrow-container").appendChild(arrowRight);
@@ -158,7 +163,7 @@ arrowLeft.textContent = "<";
 arrowRight.textContent = ">";
 
 /*creates the slideshow function */
-/*source from Lun Dev Code : https://www.youtube.com/watch?v=6PZncvhAZB8 */
+/*source from  youtube Lun Dev Code with change to function on my site */
 let slider = document.querySelector('.Projekt-rows');
 let nextBtn = document.getElementById("slide-arrow-right");
 let prevBtn = document.getElementById("slide-arrow-left");
@@ -172,7 +177,7 @@ prevBtn.onclick = () => {
 
 
 /*Creates a query media function */
-/* code source from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_matchmedia with minor changes to function on my site*/
+/* code source from w3c.school with minor changes to function on my site*/
 
 function myFunction(x) {
     if (x.matches) {
@@ -188,6 +193,7 @@ function myFunction(x) {
     }
 }
 
+/*activates the function when view-window matches the max-width */
 const x = window.matchMedia("(max-width: 720px)");
 
 myFunction(x);
